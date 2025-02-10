ac61b40f33a11355c7be210ca0a75389997833b7
alexandra-mrnts <143014934+alexandra-mrnts@users.noreply.github.com>
2025-02-10 11:44:02 +0300

    Publish photo to Telegram channel

--- amazing_space_tg_bot.py
+++ amazing_space_tg_bot.py
@@ -8,3 +8,5 @@ if __name__ == '__main__':
     token = os.environ['TG_TOKEN']
     bot = telegram.Bot(token=token)
     bot.send_message(chat_id='@cosmophoto777', text='Добро пожаловать в космос!')
+    bot.send_photo(chat_id='@cosmophoto777', photo=open('images/nasa_apod_5.jpg', 'rb'))
+    
